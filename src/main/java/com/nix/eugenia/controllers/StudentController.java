6e031cbed5b20769b6e730d5f5c0cd9aa4e7820d
vadymkiiashko@gmail.com <vadymkiiashko@gmail.com>
2020-02-20 19:45:56 +0200

    assembler

--- src/main/java/com/nix/eugenia/controllers/StudentController.java
+++ src/main/java/com/nix/eugenia/controllers/StudentController.java
@@ -5,13 +5,17 @@ import com.nix.eugenia.model.Student;
 
 import com.nix.eugenia.repositories.StudentRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.*;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+import org.springframework.hateoas.CollectionModel;
+
 
 import java.util.List;
 
 
 @RestController
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class StudentController {
 
     private final StudentRepository repository;
@@ -34,15 +38,22 @@ public class StudentController {
 
     // Single item
 
-    @GetMapping("/employees/{id}")
-    Student one(@PathVariable Long id) {
+    @GetMapping("/students/{id}")
+    EntityModel<Student> one(@PathVariable Long id) {
 
-        return repository.findById(id)
+        Student student = repository.findById(id)
                 .orElseThrow(() -> new StudentNotFoundException(id));
+
+        return new EntityModel<>(student,
+                linkTo(methodOn(StudentController.class).one(id)).withSelfRel(),
+                linkTo(methodOn(StudentController.class).all()).withRel("students"));
     }
 
+
+
+
     @PutMapping("/students/{id}")
-    Student replaceEmployee(@RequestBody Student newStudent, @PathVariable Long id) {
+    Student replaceStudent(@RequestBody Student newStudent, @PathVariable Long id) {
 
         return repository.findById(id)
                 .map(student -> {
--- src/main/java/com/nix/eugenia/exceptions/StudentNotFoundException.java
+++ src/main/java/com/nix/eugenia/exceptions/StudentNotFoundException.java
@@ -2,7 +2,7 @@ package com.nix.eugenia.exceptions;
 
 public class StudentNotFoundException extends RuntimeException {
 
-    StudentNotFoundException(Long id) {
+    public StudentNotFoundException(Long id) {
         super("Could not find student " + id + "Please go back and try again");
     }
 }
